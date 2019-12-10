ea5ded01f136f53791be773325e05d8e77e81128
USBrido <obrido@gmail.com>
2019-12-10 00:42:57 +0000

    done part I of the Shortening

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port, 8080 (might have some issues when running, just in case I forget that)
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -23,7 +23,7 @@ app.listen(PORT, () => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase}
+  let templateVars = { urls: urlDatabase }
   res.render("urls_index", templateVars);
 });
 
@@ -35,4 +35,19 @@ app.get("/urls/:shortURL", (req, res) => {
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  var length = 6,
+    allc = "0123456789abcdefghijklmnopqrstuvwxyz",
+    shortgen = '';
+  for (var i = 0; i < length; i++) {
+    shortgen += allc.charAt(Math.floor(Math.random() * allc.length));
+  }
+  return shortgen;
+};
+
+app.get("/u/:shortURL", (req, res) => {
+  // const longURL = ...
+  res.redirect(longURL);
 });
